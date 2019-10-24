3358ad5decb254868af8403670e5a8a9e876b41c
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-10-24 09:10:46 +0200

    mise à jour du chambre.h
    ajout  des getters et setters

--- chambre.h
+++ chambre.h
@@ -2,7 +2,7 @@
 * File:     chambre.h 
 * Author:   Lucie BOUCQUEMONT
 * Date:     24/10/2019 
-* Summary:  Declaration de la classe Chambre
+* Summary:  Déclaration de la classe Chambre
 */
 
 #ifndef _chambre_h
@@ -11,14 +11,26 @@
 #include <iostream>
 #include <string>
 
+using namespaces std;
+
 class Chambre{
   public:
     Chambre();
+    Chambre(int id, string type, double prix);
+    
+    double getPrixChambre();
+    string getTypeChambre();
+    int getIdChambre();
+    
+    void setPrixChambre(double prix);
+    void setTypeChambre(string type);
+  
+    string ToString();
   
   private:
-    int id;
-    string type;
-    double prix;
+    int m_id_Chambre;
+    string m_type_Chambre;
+    double m_prix_Chambre;
 
 }
 
