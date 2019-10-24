902c107067086c0f23306ab21bfbbf4b386f322f
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-10-24 09:50:54 +0200

    Création de la classe Date

---
+++ Date.h
@@ -0,0 +1,33 @@
+/** 
+* File:     chambre.h 
+* Author:   Lucie BOUCQUEMONT
+* Date:     24/10/2019 
+* Summary:  Déclaration de la classe date.h
+*/
+
+#ifndef _Date_h
+#define _Date_h
+
+#include <string>
+
+using namespace std;
+
+class Date{
+  public:
+    Date(int jour, int mois, int annee);
+    
+    int getJour();
+    int getMois();
+    int getAnnee();
+    
+    void setDate(int jour, int mois, int annee);
+    
+    string toString();
+  
+  private:
+    int m_jour;
+    int m_mois;
+    int m_annee;
+}
+
+#endif // _Date_h
