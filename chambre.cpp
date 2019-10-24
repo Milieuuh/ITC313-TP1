804de14ff9899932ab032c743849a525c88c8d11
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-10-24 08:58:11 +0200

    création de chambre.cpp
    base du fichier chambre

---
+++ chambre.cpp
@@ -0,0 +1,13 @@
+/**
+*File:     chambre.cpp 
+* Author:   Lucie BOUCQUEMONT
+* Date:     24/10/2019 
+* Summary:  Definition of the class Point
+*/
+
+#include "Chambre.h"
+
+//Constructor
+Chambre::Chambre(){
+
+}
