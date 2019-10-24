6f6c7a9b7dbb2ed8cf45a8bd4f7df2d09749edc1
Emilie GENTON <35493286+Milieuuh@users.noreply.github.com>
2019-10-24 09:09:14 +0200

    Création de Hotel.h

---
+++ hotel.h
@@ -0,0 +1,33 @@
+/** 
+  * File:     point.h 
+  * Author:   D. Ginhac (dginhac@u-bourgogne.fr)
+  * Date:     Fall 2019 
+  * Course:   C-C++ Programming / Esirem 3A Informatique Electronique 
+  * Summary:  Declaration of the class Point
+  */
+
+#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
+
+#ifndef HOTEL.H
+#define HOTEL.H
+
+class Hotel {
+  public:
+    Hotel(); 
+    string getIdHotel(); 
+    string getNom(); 
+    string getVille(); 
+    array getListeChambres(); 
+    
+  
+  private:
+    string m_id_hotel;
+    string m_nom;
+    string m_ville; 
+    array m_liste_chambres;
+}
+
+#endif // HOTEL.H
