a1a20fc07d9fdd46b612ae39e68965e2ce55027b
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-10-24 09:19:27 +0200

    mise à jour de la classe chambre

--- chambre.cpp
+++ chambre.cpp
@@ -1,13 +1,54 @@
 /**
-*File:     chambre.cpp 
+* File:     chambre.cpp 
 * Author:   Lucie BOUCQUEMONT
 * Date:     24/10/2019 
-* Summary:  Definition of the class Point
+* Summary:  Défintio de la classe Chambre
 */
 
 #include "Chambre.h"
 
-//Constructor
+//Constructors
 Chambre::Chambre(){
+    m_id_Chambre=0;
+    m_type_Chambre="nul";
+    m_prix_Chambre=0.0;
+}
 
+Chambre::Chambre(int id, string type, double prix)
+{
+    m_id_Chambre=id;
+    m_type_Chambre=type;
+    m_prix_Chambre=prix;
+}
+
+//GETTERS
+double Chambre::getPrixChambre()
+{
+  return m_id_Chambre;
+}
+
+string Chambre::getTypeChambre()
+{
+  return m_type_Chambre;
+}
+
+int Chambre::getidChambre()
+{
+  return m_id_Chambre;
+}
+
+//SETTERS
+void setPrixChambre(double prix)
+{
+  m_prix_Chambre=prix;
+}
+
+void setTypeChambre(string type)
+{
+  m_type_Chambre=type;
+}
+
+//TO STRING
+string Chambre::ToString()
+{
 }
