2d3e762dea55e09b7a1be7c0e7064a2f2d00c170
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-10-24 09:27:00 +0200

    création de client.h

---
+++ Client.h
@@ -0,0 +1,33 @@
+/** 
+* File:     chambre.h 
+* Author:   Lucie BOUCQUEMONT
+* Date:     24/10/2019 
+* Summary:  Déclaration de la classe client.h
+*/
+
+#ifndef _client_h
+#define _client_h
+
+#include <string>
+
+class Client{
+  public:
+    Client(int id,string nom, string prenom,int nbReservations=0);
+    
+    int getIdClient();
+    string getNomClient();
+    string getPrenomClient();
+    string getNbReservations();
+    
+    void setNbReservation(int nbReservation);
+    
+    string toString();
+  
+  private:
+    int m_id_Client;
+    string m_nom_Client;
+    string m_prenom_Client;
+    int m_nb_Reservations;
+}
+
+#endif // _client_h
