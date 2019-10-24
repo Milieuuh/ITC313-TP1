137d5c4c671494dda68ba9f106e171d7c804afe2
Emilie GENTON <35493286+Milieuuh@users.noreply.github.com>
2019-10-24 11:32:35 +0200

    Update chambre.cpp

--- chambre.cpp
+++ chambre.cpp
@@ -9,43 +9,43 @@
 
 //Constructors
 Chambre::Chambre(){
-    m_id_Chambre=0;
-    m_type_Chambre="nul";
-    m_prix_Chambre=0.0;
+    this->m_id_Chambre=0;
+    this->m_type_Chambre="nul";
+    this->m_prix_Chambre=0.0;
 }
 
 Chambre::Chambre(int id, string type, double prix)
 {
-    m_id_Chambre=id;
-    m_type_Chambre=type;
-    m_prix_Chambre=prix;
+    this->m_id_Chambre=id;
+    this->m_type_Chambre=type;
+    this->m_prix_Chambre=prix;
 }
 
 //GETTERS
 double Chambre::getPrixChambre()
 {
-  return m_id_Chambre;
+  return this->m_id_Chambre;
 }
 
 string Chambre::getTypeChambre()
 {
-  return m_type_Chambre;
+  return this->m_type_Chambre;
 }
 
 int Chambre::getIdChambre()
 {
-  return m_id_Chambre;
+  return this->m_id_Chambre;
 }
 
 //SETTERS
 void setPrixChambre(double prix)
 {
-  m_prix_Chambre=prix;
+  this->m_prix_Chambre=prix;
 }
 
 void setTypeChambre(string type)
 {
-  m_type_Chambre=type;
+  this->m_type_Chambre=type;
 }
 
 //TO STRING
