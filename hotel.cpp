b0c3c09349bfe9b582d8cfec7a63c9fb69ac2ed1
Emilie GENTON <35493286+Milieuuh@users.noreply.github.com>
2019-10-24 09:18:32 +0200

    Create hotel.cpp

---
+++ hotel.cpp
@@ -0,0 +1,39 @@
+/** 
+  * File:     hotel.cpp
+  * Summary:  Définition de la classe hotel
+  */
+
+
+#include <string>
+#include <iostream>
+#include "hotel.h"
+using namespace std; 
+
+Hotel::Hotel()
+{
+
+}
+
+string Hotel::getIdHotel()
+{
+
+}
+string Hotel::getNom()
+{
+
+}
+
+string Hotel::getVille()
+{
+
+}
+
+array Hotel::getListeChambres()
+{
+
+}
+
+ Hotel::ajoutChambre(Chambre ch)
+ {
+ 
+ }
