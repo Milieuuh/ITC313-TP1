d59f07432902cc606122d5b430ffe8d802cf6a86
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-11-15 09:29:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,21 +10,21 @@ int main() {
 	Hotel hotel("1", "Hotel Dieu", "Los Angeles");
 	
 
-	for(int i=0;i<10;i++)
+	for(int i=1;i<=10;i++)
 	{
-		if((i>=0)&(i<3))
+		if((i>0)&(i<=3))
 		{
-			Chambre chambre(i+1, simple, 100);
+			Chambre chambre(i, simple, 100);
 			hotel.ajoutChambre(chambre);
 		}
-		else if((i>=3)&(i<8))
+		else if((i>3)&(i<=8))
 		{
-			Chambre chambre(i+1, confort, 125);
+			Chambre chambre(i, confort, 125);
 			hotel.ajoutChambre(chambre);		
 		}
 		else
 		{
-			Chambre chambre(i+1, suite, 210);
+			Chambre chambre(i, suite, 210);
 			hotel.ajoutChambre(chambre);
 					
 		}
